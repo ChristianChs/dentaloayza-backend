92bc45acb5e66cbb8a60733785a88dedc22f7ee7
lnunezfue <114681748+lnunezfue@users.noreply.github.com>
2025-06-26 15:49:51 -0500

    feat(dental-module): Setup odontogram domain (adjusting patient reference)

--- src/dental/odontogram/entities/odontogram.entity.ts
+++ src/dental/odontogram/entities/odontogram.entity.ts
@@ -1,12 +1,12 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { BaseEntity } from '../../../common/entities/base.entity';
-
+import { Patient } from '../../../patients-management/patient/entities/patient.entity';
 @Entity('odontograms')
 export class Odontogram extends BaseEntity {
   @ApiProperty()
-  @Column({ name: 'patient_uuid' })
-  patientUuid: string;
+  @Column({ name: 'id_paciente', length: 36, unique: false })
+  idPaciente: string;
 
   @ApiProperty()
   @Column({ type: 'text', nullable: true })
@@ -15,4 +15,9 @@ export class Odontogram extends BaseEntity {
   @ApiProperty()
   @Column({ length: 50, nullable: true })
   status: string;
+
+  @ApiProperty({ type: () => Patient })
+  @ManyToOne(() => Patient, { eager: false, onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'id_paciente', referencedColumnName: 'idPaciente' })
+  patient: Patient;
 }
