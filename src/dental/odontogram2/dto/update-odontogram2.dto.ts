71686ad890cd545d4cf2187302e0acf6e95e16c2
lnunezfue <114681748+lnunezfue@users.noreply.github.com>
2025-07-08 03:43:43 -0500

    fix(odontogram): Solve wrong name Update DTO

--- src/dental/odontogram2/dto/update-odontogram2.dto.ts
+++ src/dental/odontogram2/dto/update-odontogram2.dto.ts
@@ -1,7 +1,7 @@
 import { IsOptional, IsUUID, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateOdontogramDto {
+export class UpdateOdontogramDto2 {
   @ApiProperty({ description: 'Datos del odontograma (json)', required: false })
   @IsOptional()
   @IsNotEmpty()
